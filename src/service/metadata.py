8a8d01f64377b701ee4d26efb2811d6a5fc56efd
Thorina Boenke <68156005+thorinaboenke@users.noreply.github.com>
2026-02-05 14:52:16 +0100

    Bump version from 0.1.0 to 0.1.1

--- src/service/metadata.py
+++ src/service/metadata.py
@@ -7,7 +7,7 @@ __deprecated__ = False
 __website__ = 'https://aecid.ait.ac.at'
 __license__ = 'EUPL-1.2'
 __status__ = 'Development'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = [
     '__authors__',
     '__contact__',
