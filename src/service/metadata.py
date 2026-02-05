5894cce83f65e5c5d2ce894c53ff75eb4db9f2a4
whotwagner <code@feedyourhead.at>
2026-02-05 14:18:47 +0100

    added metadata.py

---
+++ src/service/metadata.py
@@ -0,0 +1,19 @@
+__authors__ = ['Thorina Boenke', 'Anna Erdi', 'Viktor Beck', 'André García Gómez', 'Ernst Leierzopf', 'Wolfgang Hotwagner']
+__contact__ = 'aecid@ait.ac.at'
+__copyright__ = 'Copyright 2026, AIT Austrian Institute of Technology GmbH'
+__date__ = '2026/02/05'
+__deprecated__ = False
+__website__ = 'https://aecid.ait.ac.at'
+__license__ = 'EUPL-1.2'
+__status__ = 'Development'
+__version__ = '0.1.0'
+__all__ = [
+    '__authors__',
+    '__contact__',
+    '__copyright__',
+    '__date__',
+    '__deprecated__',
+    '__website__',
+    '__license__',
+    '__status__',
+    '__version__']
